8a94e61a9be1b12f3b811899e0cc10536ab75e0c
Nikita <nikita45454@gmail.com>
2018-12-04 15:20:07 +0500

    Проверил как babel преобразует цикл for of

---
+++ lesson2/es6/index.js
@@ -0,0 +1,10 @@
+import obj from './module'
+console.log(obj.toString());
+let obj1 = 'asdad';
+
+for(some of obj1) {
+    console.log(some);
+
+}
+
+//console.log(1);
--- lesson2/webpack.config.js
+++ lesson2/webpack.config.js
@@ -2,7 +2,7 @@
 let path = require('path');
 
 let conf = {
-    entry: './es6/lesson2.js',
+    entry: './es6/index.js',
     output: {
         path: path.resolve(__dirname, './js'),
         filename: 'main.js',
